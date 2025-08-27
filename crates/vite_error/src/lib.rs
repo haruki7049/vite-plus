bfeed6beed9d4aaedae025f63bd0e9c6d5ed0df1
LongYinan <lynweklm@gmail.com>
2025-08-27 21:04:39 +0800

    feat(task): allow to run local package tasks with vite run (#52)
    * feat(task): allow to run local package tasks with vite run
    
    * update doc
    
    * Refactor
    
    * return early
    
    * rebase
    
    * handle NoPackageJsonFound error
    
    * update docs for not-found cases
    
    * move matching logic to ResolvedTask::matches
    
    ---------
    
    Co-authored-by: branchseer <dk4rest@gmail.com>

--- crates/vite_error/src/lib.rs
+++ crates/vite_error/src/lib.rs
@@ -72,14 +72,17 @@ pub enum Error {
     #[error("The package.json file is not found at {0:?}")]
     PackageJsonNotFound(PathBuf),
 
-    #[error("Task not found in workspace: {0}")]
-    TaskNotFound(String),
+    #[error("Task '{task_request}' not found in workspace")]
+    TaskNotFound { task_request: String },
 
     #[error("Dependency Task '{name}' not found in package located at {package_path}")]
     TaskDependencyNotFound { name: String, package_path: String },
 
     #[error("{task_request} should not contain multiple '#'")]
     AmbiguousTaskRequest { task_request: String },
+
+    #[error("Only one task request is allowed when running in implicit mode: {0}")]
+    OnlyOneTaskRequest(String),
 
     #[error("Recursive run is not allowed when task name contains '#': {0}")]
     RecursiveRunWithScope(String),
@@ -95,6 +98,12 @@ pub enum Error {
 
     #[error("Test failed")]
     TestFailed { status: String, reason: String },
+
+    #[error("Path prefix error: {err} at {path:?}, {message}")]
+    PathPrefixError { err: std::path::StripPrefixError, message: String, path: PathBuf },
+
+    #[error("No package.json found at {0:?}")]
+    NoPackageJsonFound(PathBuf),
 
     #[error(transparent)]
     AnyhowError(#[from] anyhow::Error),
